47bd170f95dd7093895e00b367b5826af0c6a827
protajack <jackkrone@protonmail.com>
2021-07-03 18:13:09 -0500

    Create new app.js with <Team /> as home page

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,4 +1,9 @@
-// This page appears after the coach signs in, shows the coach's team
+/*
+This page appears after the coach signs in, shows the coach's team and tabs for
+tracking and practicing
+*/
+import React from 'react';
+
 
 
 export const Team = (props) => {
