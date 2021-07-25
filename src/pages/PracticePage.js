dd3d18834c8c9df01d8fe8f3fa0f02859ae26ed8
protajack <jackkrone@protonmail.com>
2021-07-25 14:24:00 -0500

    Modify node_modules webpack config file and document change in .txt file within notes directory.
    Purpose of this change was to enable the app to ignore eslint errors when starting

--- src/pages/PracticePage.js
+++ src/pages/PracticePage.js
@@ -1,7 +1,7 @@
 // This page is the rep counter
 // This is the main function of the app. It's what you use while you practice
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
@@ -9,41 +9,41 @@ import EndButton from '../components/for-practice-page/EndButton';
 import Counter from '../components/for-practice-page/Counter';
 
 export default function PracticePage(props) {
-    // set up state hook for rep numbers
-    // default repCounts is an object with a key of each technique with a value of 0
-    const [repCounts, setRepCounts] = useState(
-        () => {
-            const defaultRepState = {};
-            props.techniqueChoice.map(tech => defaultRepState[tech] = 0);
-            console.log(defaultRepState);
-            return defaultRepState;
-        }
-    );
+  // set up state hook for rep numbers
+  // default repCounts is an object with a key of each technique with a value of 0
+  const [repCounts, setRepCounts] = useState(
+    () => {
+      const defaultRepState = {};
+      props.techniqueChoice.map((tech) => defaultRepState[tech] = 0);
+      console.log(defaultRepState);
+      return defaultRepState;
+    },
+  );
 
-    return (
-        <div className="PracticePage">
-            <Header title={props.athleteChoice}> {/* athlete name needs to be passed as props */}
-                {/* <Stopwatch /> ...to be created later */}
-            </Header>
-            <Main>
-                {/* Render a Counter component for each technique chose */}
-                {props.techniqueChoice.map(
-                    (tech, index) => {
-                        return (
-                            <Counter
-                                technique={tech}
-                                key={index}
-                                repCounts={repCounts}
-                                setRepCounts={setRepCounts}
-                            />
-                        );
-                    }
-                )}
-            </Main>
-            <Footer>
-                <EndButton repCounts={repCounts}/>
-                {/*SpeechButton /> ... maybe unnecessary -- listen automatically */}
-            </Footer>
-        </div>
-    );
+  return (
+    <div className="PracticePage">
+      <Header title={props.athleteChoice}>
+        {' '}
+        {/* athlete name needs to be passed as props */}
+        {/* <Stopwatch /> ...to be created later */}
+      </Header>
+      <Main>
+        {/* Render a Counter component for each technique chose */}
+        {props.techniqueChoice.map(
+          (tech, index) => (
+            <Counter
+              technique={tech}
+              key={index}
+              repCounts={repCounts}
+              setRepCounts={setRepCounts}
+            />
+          ),
+        )}
+      </Main>
+      <Footer>
+        <EndButton repCounts={repCounts} />
+        {/* SpeechButton /> ... maybe unnecessary -- listen automatically */}
+      </Footer>
+    </div>
+  );
 }
