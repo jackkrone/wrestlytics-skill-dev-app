61cad69874b583a471b021396b375bf71c9d5402
protajack <jackkrone@protonmail.com>
2021-07-30 18:16:31 -0500

    Set up GET request for TeamPage

--- server/index.js
+++ server/index.js
@@ -13,22 +13,99 @@ const pool = require('./db');
 const app = express();
 
 // Middleware
-//app.use(cors());  // recall: cors enabls cross-origin resource sharing
-//app.use(express.json()); // enables use of req.body
+app.use(cors());  /* makes server accessible to any domain requesting resources via a browser
+                  https://stackoverflow.com/questions/46024363/what-does-app-usecors-do#46024491 */
+app.use(express.json()); // instructs app to parse all incoming requests with JSON payloads. Parsed object saved at req.body.
 
 // ROUTES //
 //========//
 
 // Create/POST a practice session
-    // THIS IS TOP PRIORITY!
+/*
+app.post('/practice'
+  (req, res, next) => {
+    try {
+      // set up async query
+      const { teamId, athleteId } = req.body; // not 100% confident this is constructed correctly
+      
+      const addPractice = await pool.query(
+        `INSERT INTO practices (team_id, athlete_id)
+         VALUES ($1, $2)
+         RETURNING *`,
+        [teamId, athleteId]
+      )
+      
+      // res.json(addPractice) // .json() converts to json and calls res.send()
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+*/
 
+// Read/GET athlete names and technique list
+app.get('/',
+  async (req, res) => {
+    // Set Team ID manually for now, in future could be based on session or cookie variables
+    const teamId = 2;
+    
+    try {
+      // Query athlete ids, then athlete names
+      const athletesIds = await pool.query(
+        `SELECT athlete_id
+         FROM athletes_teams
+         WHERE team_id=$1`,
+        [teamId]
+      );
+      
+      let athletesList = [];
+      for (elem of athletesIds.rows) {
+        const athleteName = await pool.query(
+          `SELECT first_name
+           FROM athletes
+           WHERE athlete_id=$1`,
+          [elem.athlete_id]
+        );
+        const id = elem.athlete_id;
+        const name = athleteName.rows[0].first_name;
+        athletesList.push({id, name});
+      }
 
+      // Query activity id, then techniques
+      let activityId = await pool.query(
+        `SELECT activity_id
+         FROM teams
+         WHERE team_id=$1`,
+        [teamId]
+      );
+
+      activityId = activityId.rows[0].activity_id; // changes activityId to a simple integer variable
+      let techniquesList = await pool.query(
+        `SELECT technique_id, technique_name
+         FROM techniques
+         WHERE activity_id=$1`,
+        [activityId]
+      );
+      
+      techniquesList = techniquesList.rows; // changes techniquesList to array of objects like Athletes list
+    
+      // Send response
+      infoForTeamPage  = {teamId, activityId, techniquesList, athletesList};
+      res.json(infoForTeamPage); // Needs to combine athletesList and techniquesList
+    } catch (err) {
+      console.error(err.message);
+    }
+
+    
+
+  }
+);
 
 // Read/GET practice data
     // For a single athlete
     // For entire team
 // Update/PUT ...
 // Delete/DELETE ...
+
 
 // Start server
 app.listen(5000, () => {console.log('wrestlytics server has started on port 5000')});
