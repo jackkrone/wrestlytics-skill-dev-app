634f2ffe454851e72a8ab4418321b29580666ea5
protajack <jackkrone@protonmail.com>
2021-08-03 15:58:50 -0500

    Create POST route associated with PracticePage

--- server/routes/practicePageRouter.js
+++ server/routes/practicePageRouter.js
@@ -5,22 +5,45 @@ const express = require('express');
 const pool = require('../db');  
 
 const practicePageRouter = express.Router();
+practicePageRouter.use(express.json()); // instructs app to parse all incoming requests with JSON payloads. Parsed object saved at req.body
 
-practicePageRouter.post('/practice'
-  (req, res, next) => {
+practicePageRouter.post('',
+  async (req, res) => {
     try {
       // set up async query
-      const { teamId, athleteId } = req.body; // not 100% confident this is constructed correctly
+      const { teamId, athleteId, repsArray } = req.body;
       
       const addPractice = await pool.query(
         `INSERT INTO practices (team_id, athlete_id)
-         VALUES ($1, $2)
-         RETURNING *`,
-        [teamId, athleteId]
-      )
+         VALUES ($1, $2)`,
+        [teamId, athleteId],
+        (err, results) => {
+          // throw error
+          if (err) throw err;
+
+          // Insert rows into practices_techniques
+          const practiceId = results.rows[0].practice_id;
+          repsArray.forEach(
+            async (elem) => {
+              await pool.query(
+                `INSERT INTO practices_techniques (practice_id, technique_id, reps)
+                 VALUES ($1, $2, $3)`,
+                [practiceId, elem.id, elem.reps],
+                (err, results) => {
+                  if (err) throw err;
+                  console.log(results.rows); // logs practices_techniques rows
+                },
+              );
+            },
+          );
+        },
+      );
       
-      // res.json(addPractice) // .json() converts to json and calls res.send()
+        res.status(200).send('POST Success');
     } catch (err) {
       console.error(err.message);
     }
-  });
+  },
+);
+
+module.exports = practicePageRouter;
