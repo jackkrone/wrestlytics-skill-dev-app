fe5e9479e163433ab6a2dc695f0db76d121c340a
protajack <jackkrone@protonmail.com>
2021-08-10 10:04:36 -0500

    Pad core components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 // The footer's main rendering job is to render it's children
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 export default function Footer({ children }) {
   return (
-    <div className="Footer">
+    <Box py={2}>
       {children}
-    </div>
+    </Box>
   );
 }
