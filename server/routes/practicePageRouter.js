f004639e27f029c1fe7014ab51057b650ffabe38
jackkrone <jackkrone@protonmail.com>
2021-09-27 12:09:11 -0500

    Get rid of array methods with async functions

--- server/routes/practicePageRouter.js
+++ server/routes/practicePageRouter.js
@@ -19,21 +19,19 @@ practicePageRouter.post('',
          RETURNING *`,
         [teamId, athleteId],
       );
-      
+      console.log(addPractice);
+
       // Insert rows into practices_techniques
-      console.log(addPractice); // log this to see if it looks correct
       const practiceId = addPractice.rows[0].practice_id;
-      repsArray.forEach(
-        async (elem) => {
-          const addReps = await pool.query(
-            `INSERT INTO practices_techniques (practice_id, technique_id, reps)
-             VALUES ($1, $2, $3)
-             RETURNING *`,
-            [practiceId, elem.id, elem.reps],
-          );
-          console.log(addReps.rows[0]); // logs the practices_techniques rows added
-        },
-      );
+      for (elem of repsArray) {
+        const addReps = await pool.query(
+          `INSERT INTO practices_techniques (practice_id, technique_id, reps)
+          VALUES ($1, $2, $3)
+          RETURNING *`,
+          [practiceId, elem.id, elem.reps],
+        );
+        console.log(addReps.rows[0]); // logs the practices_techniques rows added
+      }
       
       res.status(200).send('POST Success');
     } catch (err) {
